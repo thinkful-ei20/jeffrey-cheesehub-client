adcef0689d2a3f11e329769d78e5e4dc29106b96
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-25 16:48:41 -0500

    Render the React component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CheeseList from './components/cheese-list';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cheeses = ["Bath Blue", "Barkham Blue", "Buxton Blue"];
+
+ReactDOM.render(
+  <CheeseList cheeses={cheeses} />,
+  document.getElementById('root'));
 registerServiceWorker();
